475a176cdd38a7ef80f9f258a8e684bac83ec479
felixlaynardi <felixxlay@gmail.com>
2021-07-24 18:40:23 +0700

    update to receive updated_metadata from payload

--- backend/data/api.py
+++ backend/data/api.py
@@ -3,7 +3,7 @@ import requests
 import json
 
 def fetch_data():
-    request_payload = payload.create_payload()
+    request_payload, updated_metadata = payload.create_payload()
     response = requests.get(request_payload.get('url'), params=request_payload.get('params'))
     
-    return response.json()
+    return response.json(), updated_metadata
--- backend/data/main.py
+++ backend/data/main.py
@@ -5,8 +5,8 @@ if __name__ == '__main__':
     # Generate table
     db.generate_table()
 
-    # Fetch data from api
-    data = api.fetch_data()
+    # Fetch data from api and updated metadata
+    data, updated_metadata  = api.fetch_data()
 
-    # Upsert data into db
-    db.upsert_data(data)
+    # Upsert data into db and update metadata
+    db.upsert_data(data, updated_metadata)
