9578c87454999551772ea5cb1e2ae825646f936b
felixlaynardi <felixxlay@gmail.com>
2021-07-24 18:40:08 +0700

    refactor fetch_by_country to fetch_by_time, updated to return updated_metadata

--- backend/data/payload.py
+++ backend/data/payload.py
@@ -4,7 +4,7 @@ from os import error
 
 def create_payload():
     # Fetch metadata
-    with open('metadata.txt') as f:
+    with open('metadata.txt', 'r') as f:
         contents = f.read()
         metadata = ast.literal_eval(contents)
     date = metadata.get('from')
@@ -13,7 +13,7 @@ def create_payload():
     if(date == '-1'):
         return fetch_day_one()
     else:
-        return fetch_by_country(datetime.datetime.strptime(date, '%Y-%m-%dT%H:%M:%S.%f'))
+        return fetch_by_time(datetime.datetime.strptime(date, '%Y-%m-%d'))
 
 def fetch_day_one():
     # Set up payload
@@ -21,9 +21,13 @@ def fetch_day_one():
     payload['params'] = {}
     payload['url'] = 'https://api.covid19api.com/dayone/country/indonesia'
 
-    return payload
+    # Create updated metadata
+    current_date = datetime.datetime.now().strftime('%Y-%m-%d')
+    updated_metadata = f"{{'from':'{current_date}'}}"
 
-def fetch_by_country(from_date):
+    return payload, updated_metadata
+
+def fetch_by_time(from_date):
     # Set up date params
     to_date = datetime.datetime.now() - datetime.timedelta(hours=8)
     from_date_param = from_date.isoformat()
@@ -39,4 +43,8 @@ def fetch_by_country(from_date):
     payload['params'] = {'from':from_date_param,'to':to_date_param}
     payload['url'] = 'https://api.covid19api.com/country/indonesia'
 
-    return payload
+    # Create updated metadata
+    current_date = to_date.strftime('%Y-%m-%d')
+    updated_metadata = f"{{'from':'{current_date}'}}"
+
+    return payload, updated_metadata
