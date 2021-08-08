fe050ca66a4364f03d42eb41737a5cdb9ede78c1
felixlaynardi <felixxlay@gmail.com>
2021-08-09 02:39:47 +0700

    create landingcase, landingbox, lottieplayer

---
+++ frontend/src/components/LandingBox/LandingBox.js
@@ -0,0 +1,30 @@
+import './LandingBox.css';
+
+import LandingCase from './LandingCase/LandingCase.js'
+import LottiePlayer from '../LottiePlayer/LottiePlayer';
+
+import virusAnimation from '../../assets/virus.json'
+
+export default function LandingBox(){
+    return(
+        <div className="landing-box-container">
+        <div className="landing-box-inner">
+          <div className="landing-box-top">
+            <div className="landing-box-top-left">
+              <div className="welcome-text">
+                Welcome <span className="welcome-name">Nael</span>! <br/>
+                <span className="welcome-subtitle">Let's Help Recover the Earth</span>
+              </div>
+              <button className="dashboard-button">
+                Dashboard
+              </button>
+            </div>
+            <div className="landing-box-top-right">
+              <LottiePlayer animation={virusAnimation} animationClass={"virus-animation"}/>
+            </div>
+          </div>
+          <LandingCase/>
+        </div>
+      </div>
+    )
+}
