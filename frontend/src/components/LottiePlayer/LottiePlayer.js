9961bc4d3b70b4894dff0262466c12990062569b
felixlaynardi <felixxlay@gmail.com>
2021-08-09 23:22:15 +0700

    clean code

--- frontend/src/components/LottiePlayer/LottiePlayer.js
+++ frontend/src/components/LottiePlayer/LottiePlayer.js
@@ -3,12 +3,7 @@ import Lottie from 'react-lottie-player'
  
 const LottiePlayer = (props) => {
   return (
-    <Lottie
-      className={props.animationClass}
-      loop
-      animationData={props.animation}
-      play
-    />
+    <Lottie className={props.animationClass} loop animationData={props.animation} play/>
   )
 }
 
