1e85c087f31d4d4f0103e2cc324733de30a90442
felixlaynardi <felixxlay@gmail.com>
2021-08-17 17:03:03 +0700

    add navlink to navbar and switch to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,48 @@ import React from 'react';
 
 import './App.css';
 
-import LandingPage from './components/LandingPage/LandingPage';
 import Navbar from './components/Navbar/Navbar';
+import LandingPage from './components/LandingPage/LandingPage';
+import DashboardPage from './components/DashboardPage/DashboardPage';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+const routes = [
+  {
+    path: "/",
+    exact: true,
+    content: () => <LandingPage/>
+  },
+  {
+    path: "/dashboard",
+    content: () => <DashboardPage/>
+  },
+  {
+    path: "/data"
+  }
+];
 
 function App() {
   return (
     <div className="App">
       <React.StrictMode>
-        <Navbar/>
-        <LandingPage/>
+        <Router>
+          <Navbar/>
+          <Switch>
+              {routes.map((route, index) => (
+                <Route
+                  key={index}
+                  path={route.path}
+                  exact={route.exact}
+                  children={route.content}
+                />
+              ))}
+          </Switch>
+        </Router>
       </React.StrictMode>
     </div>
   );
--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,43 +1,37 @@
 import './Navbar.css';
 
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-} from "react-router-dom";
-  
+import {NavLink} from "react-router-dom";
 
 function Navbar() {
   return (
-    <Router>
+    <div>
         <nav className="sidebar-container">
             <ul className="sidebar-list-box">
                 <li className="sidebar-list">
-                    <Link className="icon-home" to="/"></Link>
+                    <NavLink className="icon-home" activeClassName="active" exact to="/"></NavLink>
                 </li>
                 <li className="sidebar-list">
-                    <Link className="icon-columns" to="/about"></Link>
+                    <NavLink className="icon-bar-chart" activeClassName="active" to="/dashboard"></NavLink>
                 </li>
                 <li className="sidebar-list">
-                    <Link className="icon-bar-chart" to="/users"></Link>
+                    <NavLink className="icon-th-list" activeClassName="active" to="/data"></NavLink>
                 </li>
             </ul>
         </nav>
         <nav className="topbar-container">
             <ul className="topbar-list-box">
                 <li className="topbar-list">
-                    <Link className="icon-home" to="/"></Link>
+                    <NavLink className="icon-home" activeClassName="active" exact to="/"></NavLink>
                 </li>
                 <li className="topbar-list">
-                    <Link className="icon-columns" to="/about"></Link>
+                    <NavLink className="icon-bar-chart" activeClassName="active" to="/dashboard"></NavLink>
                 </li>
                 <li className="topbar-list">
-                    <Link className="icon-bar-chart" to="/users"></Link>
+                    <NavLink className="icon-th-list" activeClassName="active" to="/data"></NavLink>
                 </li>
             </ul>
         </nav>
-    </Router>
+    </div>
   );
 }
 
