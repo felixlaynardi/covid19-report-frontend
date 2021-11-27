df05f16bfa884657ab2fce3fa7f99f7db286a17c
felixlaynardi <61959808+felixlaynardi@users.noreply.github.com>
2021-11-27 16:29:40 +0700

    Remove backend from repo (#4)

--- backend/data/lib/api.py
+++
@@ -1,9 +0,0 @@
-import lib.payload as payload
-import requests
-import json
-
-def fetch_data():
-    request_payload, updated_metadata = payload.create_payload()
-    response = requests.get(request_payload.get('url'), params=request_payload.get('params'))
-    
-    return response.json(), updated_metadata
