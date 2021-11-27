03f187179df358c18d3885767b9f8c67ac009856
felixlaynardi <61959808+felixlaynardi@users.noreply.github.com>
2021-11-27 15:46:27 +0700

    Change Repo to Ionic (#3)

---
+++ frontend/src/App.tsx
@@ -0,0 +1,46 @@
+import { Redirect, Route } from 'react-router-dom';
+import { IonApp, IonRouterOutlet } from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
+
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/react/css/core.css';
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
+
+/* Theme variables */
+import './theme/variables.css';
+
+import LandingPage from './pages/LandingPage/LandingPage';
+import DashboardPage from './pages/DashboardPage/DashboardPage';
+
+import { Switch } from 'react-router-dom';
+
+import './index.css'
+
+const App: React.FC = () => (
+  <IonApp>
+    <IonReactRouter>
+      <IonRouterOutlet id="main">
+        <Switch>
+          <Route path="/home" component={LandingPage}/>
+          <Route path="/dashboard" component={DashboardPage}/>
+          <Route path="/data" component={DashboardPage}/>
+          <Redirect exact from="/" to="/home"/>
+        </Switch>
+      </IonRouterOutlet>
+    </IonReactRouter>
+  </IonApp>
+);
+
+export default App;
--- frontend/src/pages/DashboardPage/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage/DashboardPage.tsx
@@ -1,7 +1,12 @@
+import Navbar from '../../components/Navbar/Navbar';
+
 export default function DashboardPage(){
     return(
+      <>
+      <Navbar/>
       <div className="landing-box-container">
         
       </div>
+      </>
     )
 }
