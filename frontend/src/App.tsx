234966a9566a934c425507c4a240bedec68bf83d
Gracia Angelica N <61301424+grrrracia@users.noreply.github.com>
2021-12-07 06:26:24 +0700

    Added features
    - Added get current location and display on profile page
    - Added dark mode
    - Prepared input for API connection
    - Updated profile page to save data input

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -29,6 +29,8 @@ import ProfileBefore from './pages/ProfilePage/ProfileBefore';
 import { Switch } from 'react-router-dom';
 
 import './index.css'
+import ProfilePage from "./pages/ProfilePage/ProfilePage";
+import EditProfile from "./pages/EditProfilePage/EditProfile";
 
 const App: React.FC = () => (
   <IonApp>
@@ -38,7 +40,10 @@ const App: React.FC = () => (
           <Route path="/home" component={LandingPage}/>
           <Route path="/dashboard" component={DashboardPage}/>
           <Route path="/data" component={DataPage}/>
-          <Route path="/profile" component={ProfileBefore}/>
+          {/*<Route path="/profile" component={ProfileBefore}/>*/}
+          <Route path="/profile" component={ProfilePage}/>
+          <Route path="/editProfile" component={EditProfile}/>
+          <Route path="/myprofile" component={ProfilePage}/>
           <Redirect exact from="/" to="/home"/>
         </Switch>
       </IonRouterOutlet>
--- frontend/src/pages/EditProfilePage/EditProfile.tsx
+++ frontend/src/pages/EditProfilePage/EditProfile.tsx
@@ -6,7 +6,7 @@ import {
     IonPage,
     IonToolbar,
     IonBackButton,
-    IonLabel, IonButton, IonInput, IonItem, IonIcon
+    IonLabel, IonButton, IonInput, IonItem, IonIcon, isPlatform, IonDatetime
 } from "@ionic/react";
 import './EditProfile.css';
 import {calendar, fitness, mail, person} from "ionicons/icons";
@@ -14,8 +14,23 @@ import {useState} from "react";
 import {Camera, CameraResultType, CameraSource} from '@capacitor/camera';
 // import {Directory, Filesystem} from "@capacitor/filesystem";
 
-
+import './EditProfile.css';
 const EditProfile: React.FC = () => {
+
+
+    const[myFullName, setMyFullName] = useState<string>('John Doe');
+    const[myProfilePicture, setMyProfilePicture] = useState<string>('https://i.ibb.co/WHrxLMF/profilepic.png');
+    const[myVaccineStatus, setMyVaccineStatus] = useState<string>('Vaccine Undocumented');
+    const[myVaccineType, setMyVaccineType] = useState<string>('');
+    const[myHealthStatus, setMyHealthStatus] = useState<string>('Unknown Health Status');
+    const[myLastHealthCheck, setMyLastHealthCheck] = useState<number>(0);
+
+    const[myDateOfBirth, setMyDateOfBirth] = useState(new Date());
+    const[myEmail, setMyEmail] = useState<string>();
+
+    const[myCerti1, setMyCerti1] = useState<string>('https://i.ibb.co/RbzWjSL/blank.png');
+    const[myCerti2, setMyCerti2] = useState<string>('https://i.ibb.co/RbzWjSL/blank.png');
+
 
     const [takenPhoto, setTakenPhoto] = useState<{
         path: string;
@@ -41,20 +56,30 @@ const EditProfile: React.FC = () => {
         });
     };
 
+
+    const saveDataHandler = async () => {
+        console.log({myDateOfBirth});
+        console.log({myFullName});
+        console.log({myEmail});
+        console.log({myVaccineType});
+    }
+
     return (
         <IonPage>
             <IonHeader class="ion-no-border">
                 <IonToolbar>
                     <IonButtons slot="start">
                         <IonBackButton defaultHref="/profile"/>
-                        <IonLabel class="backText">Back</IonLabel>
+                        {isPlatform('android') && (
+                            <IonLabel class="backText">Back</IonLabel>
+                        )}
                     </IonButtons>
                 </IonToolbar>
             </IonHeader>
             <IonContent fullscreen>
                 <IonCard>
                     <div className="editProfileContainer">
-                        <img src="https://i.ibb.co/WHrxLMF/profilepic.png"/>
+                        <img src={myProfilePicture}/>
                         <IonButton expand="full" fill="clear" className="changePhotoButton" onClick={takePhotoHandler}>Change Photo</IonButton>
                     </div>
                     <div className="editTitles">
@@ -62,27 +87,29 @@ const EditProfile: React.FC = () => {
                     </div>
                     <div>
                         <IonItem>
-                            <IonInput placeholder="FULL NAME" class="editNameInput"><IonIcon class="nameInputIcon" icon={person} slot="start"/></IonInput>
+                            <IonInput placeholder="FULL NAME" class="editProfileInput" onIonInput={(e: any) => setMyFullName(e.target.value)} value={myFullName}><IonIcon class="inputIcon" icon={person} slot="start"/></IonInput>
                         </IonItem>
                         <IonItem>
-                            <IonInput placeholder="EMAIL" class="emailInput"><IonIcon class="emailInputIcon" icon={mail} slot="start"/></IonInput>
+                            <IonInput placeholder="EMAIL" class="editProfileInput" onIonInput={(e: any) => setMyEmail(e.target.value)} value={myEmail}><IonIcon class="inputIcon" icon={mail} slot="start"/></IonInput>
                         </IonItem>
                         <IonItem>
-                            <IonInput placeholder="DATE OF BIRTH" class="nameInput" type="date"><IonIcon class="emailInputIcon" icon={calendar} slot="start"/></IonInput>
+                            <IonInput placeholder="DATE OF BIRTH" class="editProfileInput" type="date" onIonChange={(e: any) => setMyDateOfBirth(e.target.value)}><IonIcon class="inputIcon" icon={calendar} slot="start"/></IonInput>
                         </IonItem>
                         <IonItem>
-                            <IonInput placeholder="VACCINE TYPE" class="nameInput"><IonIcon class="emailInputIcon" icon={fitness} slot="start"/></IonInput>
+                            <IonInput placeholder="VACCINE TYPE" class="editProfileInput" onIonInput={(e: any) => setMyVaccineType(e.target.value)} value={myVaccineType}><IonIcon class="inputIcon" icon={fitness} slot="start"/></IonInput>
                         </IonItem>
                     </div>
                     <div className="certificateTitle">
                         <h1>Certificates:</h1>
                     </div>
                     <div className="certificates">
-                        <img src="https://i.ibb.co/XZYNHdt/serti.jpg"/>
+                        <img src={myCerti1}/>
                         <IonButton expand="full" fill="clear" className="changePhotoButton" onClick={takePhotoHandler}>Upload Certificate 1</IonButton>
-                        <img src="https://i.ibb.co/XZYNHdt/serti.jpg"/>
+                        <img src={myCerti2}/>
                         <IonButton expand="full" fill="clear" className="changePhotoButton" onClick={takePhotoHandler}>Upload Certificate 2</IonButton>
                     </div>
+
+                    <IonButton expand="full" fill="clear" className="saveButton" onClick={saveDataHandler}>SAVE</IonButton>
                 </IonCard>
             </IonContent>
         </IonPage>
