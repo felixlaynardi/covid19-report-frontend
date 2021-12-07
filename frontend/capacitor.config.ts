4e0dfbc960fa08934ccd74c00a6e37d9dedac871
felixlaynardi <61959808+felixlaynardi@users.noreply.github.com>
2021-12-08 01:27:20 +0700

    fixing android

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'frontend',
+  appName: 'covid19-personal-tracker',
   webDir: 'build',
   bundledWebRuntime: false
 };
